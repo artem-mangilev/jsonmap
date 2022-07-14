8620febe8baa235b0b199c8dca16bf27bd7649aa
amangilev <artmnglv@gmail.com>
2022-07-14 20:54:38 +0500

    Add only compiled files into build

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,28 @@
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
 export default [
     {
         input: './src/index.ts',
         output: {
-            file: 'bundle.js',
+            file: 'index.js',
             format: 'cjs'
         },
         plugins: [
             typescript({ target: 'ES6' }),
             commonjs()
         ]
-    }
+    },
+    {
+        input: './src/index.ts',
+        output: {
+            file: 'index.d.ts',
+            format: 'cjs'
+        },
+        plugins: [
+            typescript(), 
+            dts()
+        ]
+      },
 ]
