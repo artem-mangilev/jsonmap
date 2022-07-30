828245013d5eeb93aa9e49043a985f5e00ef90ff
amangilev <artmnglv@gmail.com>
2022-07-30 19:18:17 +0500

    Make getting test jsons async

--- test/main.test.js
+++ test/main.test.js
@@ -2,40 +2,42 @@ const { JsonMap } = require('../index');
 const fs = require('fs');
 const path = require('path')
 
+const readFile = (pathStr) => fs.promises.readFile(path.resolve(__dirname, pathStr))
+
 function getTestJsons(name) {
-    return [
-        fs.readFileSync(path.resolve(__dirname, `./json/${name}/${name}.input.json`), 'utf-8'),
-        fs.readFileSync(path.resolve(__dirname, `./json/${name}/${name}.transformer.json`), 'utf-8'),
-        fs.readFileSync(path.resolve(__dirname, `./json/${name}/${name}.result.json`), 'utf-8')
-    ]
+    return Promise.all([
+        readFile(`./json/${name}/${name}.input.json`),
+        readFile(`./json/${name}/${name}.transformer.json`),
+        readFile(`./json/${name}/${name}.result.json`)
+    ])
 }
 
-test('evaluate #valueof', () => {
-    const [input, transformer, expectedResult] = getTestJsons('simple-value-of');
+test('evaluate #valueof', async () => {
+    const [input, transformer, expectedResult] = await getTestJsons('simple-value-of');
 
     expect(JSON.parse(new JsonMap().transform(input, transformer))).toEqual(JSON.parse(expectedResult))
 })
 
-test('evaluate #ifcondition', () => {
-    const [input, transformer, expectedResult] = getTestJsons('simple-if-condition');
+test('evaluate #ifcondition', async () => {
+    const [input, transformer, expectedResult] = await getTestJsons('simple-if-condition');
 
     expect(JSON.parse(new JsonMap().transform(input, transformer))).toEqual(JSON.parse(expectedResult))
 })
 
-test('evaluate array #loop', () => {
-    const [input, transformer, expectedResult] = getTestJsons('simple-array-loop');
+test('evaluate array #loop', async () => {
+    const [input, transformer, expectedResult] = await getTestJsons('simple-array-loop');
 
     expect(JSON.parse(new JsonMap().transform(input, transformer))).toEqual(JSON.parse(expectedResult))
 })
 
-test('evaluate several array #loop`s', () => {
-    const [input, transformer, expectedResult] = getTestJsons('several-array-loops');
+test('evaluate several array #loop`s', async () => {
+    const [input, transformer, expectedResult] = await getTestJsons('several-array-loops');
 
     expect(JSON.parse(new JsonMap().transform(input, transformer))).toEqual(JSON.parse(expectedResult))
 })
 
-test('evaluate access to array item with index', () => {
-    const [input, transformer, expectedResult] = getTestJsons('index-access');
+test('evaluate access to array item with index', async () => {
+    const [input, transformer, expectedResult] = await getTestJsons('index-access');
 
     expect(JSON.parse(new JsonMap().transform(input, transformer))).toEqual(JSON.parse(expectedResult))
 })
